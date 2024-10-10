ee408f13c9910ecfc50e7961479ceb17b3578c4d
MHWillard <mhwillard@gmail.com>
2024-10-10 16:12:29 -0400

    build posting fuctionality

--- obj/Debug/net6.0/vibe-log-backend.AssemblyInfo.cs
+++ obj/Debug/net6.0/vibe-log-backend.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("vibe-log-backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+91f8e082ef796641e023cc4a667e151de19b05aa")]
 [assembly: System.Reflection.AssemblyProductAttribute("vibe-log-backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("vibe-log-backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
