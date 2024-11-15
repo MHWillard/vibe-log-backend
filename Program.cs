2368d91c860c6efe56f5977801efde12b54d35d8
MHWillard <mhwillard@gmail.com>
2024-11-15 11:05:00 -0500

    .

--- Program.cs
+++ Program.cs
@@ -7,15 +7,12 @@ using System.Reflection.Metadata.Ecma335;
 
 var MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
 var builder = WebApplication.CreateBuilder(args);
+
 builder.Configuration.AddEnvironmentVariables();
-
-//var connectionString = "Host=postgres.railway.internal;Port=5432;Database=railway;Username=postgres;Password=YHDbYyQeHrnnhJEtoZCngCUuenoSfbsC";
 var connectionString = Environment.GetEnvironmentVariable("CONNECTIONSTRING");
-
 
 builder.Services.AddDbContextPool<FeedContext>(opt =>
     opt.UseNpgsql(connectionString));
-
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen(c =>
@@ -39,12 +36,6 @@ builder.Services.AddCors(options =>
 
 var app = builder.Build();
 app.UseCors(MyAllowSpecificOrigins);
-
-/*
- set up psql as if running locally
-change out envs to match internet ones
-test routes
- */
 
 //if (app.Environment.IsDevelopment())
 //{
@@ -103,23 +94,5 @@ app.MapPost("/new-post", async (FeedContext db, HttpContext ctx) =>
 });
 
 app.MapGet("/post/{post_id}", async (FeedContext db, int post_id) => await db.posts.FindAsync(post_id));
-
-app.MapGet("/test-post", () => new
-{
-        post_table_id = 1,
-        user_id = 1234,
-        content = "Returning a test post",
-        post_id = 5678,
-        post_date = DateTime.UtcNow
-});
-
-app.MapGet("/test-posts", () => new[]
-{
-        new { post_table_id = 18, user_id = 1234, content = "happy", post_id = 5678, post_date = "2024-10-20T14:00:00Z" },
-        new { post_table_id = 19, user_id = 1234, content = "sad", post_id = 6789, post_date = "2024-10-20T18:30:00Z" },
-        new { post_table_id = 20, user_id = 1234, content = "excited", post_id = 7890, post_date = "2024-10-20T22:45:00Z" },
-        new { post_table_id = 21, user_id = 1234, content = "angry", post_id = 8901, post_date = "2024-10-21T13:15:00Z" },
-        new { post_table_id = 22, user_id = 1234, content = "calm", post_id = 9012, post_date = "2024-10-21T20:00:00Z" }
-});
 
 app.Run();
